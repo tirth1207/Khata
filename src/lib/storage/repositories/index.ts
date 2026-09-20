8bba212ca661741f25e8ccc15f6766edc218ab90
Tirth Rathod <rathodtirth1207@gmail.com>
2026-09-20 19:49:56 +0530

    fix

---
+++ src/lib/storage/repositories/index.ts
@@ -0,0 +1,12 @@
+// Khata - Repositories Index
+// Export all repositories
+
+export * from './accounts';
+export * from './transactions';
+export * from './categories';
+export * from './budgets';
+export * from './goals';
+export * from './bills';
+export * from './debts';
+export * from './investments';
+export * from './recurring';
